dd400e91bcfef96c6ee5e525ab25fa18d765af4e
LucioFider <fidelislucio404@gmail.com>
2023-03-31 10:43:10 +1000

    Testing out components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,3 @@
-import React from "react";
-import { Box, HStack, Image, Link } from "@chakra-ui/react";
-import logo from "../assets/react.svg";
-import { MdCheckCircle, MdSettings } from "react-icons/md";
-
-const Navbar = () => {
-  return (
-    <HStack justifyContent="space-between">
-      <Image src={logo} boxSize="30px" />
-      <Box justifyContent="space-between" spacing={5}>
-        <Link>Home</Link>
-        <Link>Projects</Link>
-        <Link>Features</Link>
-        <Link>Pricing</Link>
-        <Link>Contact</Link>
-      </Box>
-    </HStack>
-  );
-};
-
-export default Navbar;
+export default function Navbar() {
+  return <div>Navbar</div>;
+}
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import { Box, Container, Heading, Text } from "@chakra-ui/react";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const boxStyles = {
+    p: "10px",
+    bg: "purple.400",
+    color: "white",
+    m: "10px",
+    textAlign: "center",
+    filter: "blur(2px)",
+  };
+  return (
+    <Container as="section" maxWidth="4xl" py="20px">
+      <Heading marginY="30px" p="10px">
+        Chakra UI Component
+      </Heading>
+      <Text ml="30px">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus,
+        itaque esse odio ut dolore, ea nostrum velit distinctio non quasi
+        aliquam adipisci repellat natus. Sapiente dignissimos accusamus magnam
+        ab. Tempore. z
+      </Text>
+      <Text ml="30px" color="blue.300" fontWeight="bold">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium
+        dolor blanditiis deserunt cum error minima, molestiae officiis debitis
+        dolore? Dignissimos eius quidem assumenda ab excepturi tenetur
+        aspernatur blanditiis ratione accusantium!
+      </Text>
+      <Box my="30px" p="20px" bg="orange">
+        <Text>This is box </Text>
+      </Box>
+
+      <Box sx={boxStyles}>Hello Ninjas</Box>
+    </Container>
+  );
 };
 
 export default Dashboard;
