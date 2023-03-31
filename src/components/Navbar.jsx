b49bcd684104f161bf8d7184daeb8a1ff9f4b94a
LucioFider <fidelislucio404@gmail.com>
2023-03-31 10:58:03 +1000

    Create Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,40 @@
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  HStack,
+  Spacer,
+  Text,
+} from "@chakra-ui/react";
+
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <Flex as="nav" p="10px" alignItems="center">
+      <Heading as="h1">Solo Tasks</Heading>
+      <Spacer />
+
+      <HStack spacing="20px">
+        <Box bg="gray.200" p="10px" color="black" borderRadius="20px">
+          M
+        </Box>
+        <Text>john@gmm.dev</Text>
+        <Button colorScheme="purple">Logout</Button>
+      </HStack>
+    </Flex>
+    // <Flex bg="gray.200" justify="space-between" wrap="wrap" gap="2">
+    //   <Box w="150px" h="50px" bg="red">
+    //     1
+    //   </Box>
+    //   <Box w="150px" h="50px" bg="blue">
+    //     2
+    //   </Box>
+    //   <Box w="150px" h="50px" flexGrow="1" bg="green">
+    //     3
+    //   </Box>
+    //   <Box w="150px" h="50px" flexGrow="2" bg="yellow">
+    //     4
+    //   </Box>
+    // </Flex>
+  );
 }
